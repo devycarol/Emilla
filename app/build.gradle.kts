4119f7cc25e367ef88d8ad25e45845aab83469d0
devycarol <[email protected]>
2025-03-18 13:10:12 -0600

    release alpha 1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "net.emilla.nebula"
         minSdk = 21
         targetSdk = 35
-        versionCode = 5
-        versionName = "Alpha 1.6"
+        versionCode = 6
+        versionName = "Alpha 1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
