c2f58817476e2ce74e5df40c7e31106eec254325
devycarol <[email protected]>
2025-03-27 09:36:55 -0600

    Features can be in Kotlin

--- app/src/main/java/net/emilla/util/Features.kt
+++ app/src/main/java/net/emilla/util/Features.kt
@@ -1,25 +1,31 @@
-package net.emilla.util;
+@file:JvmName("Features")
 
-import android.content.pm.PackageManager;
-import android.os.Build;
+package net.emilla.util
 
-import androidx.annotation.RequiresApi;
+import android.content.pm.PackageManager
+import android.os.Build
 
-public final class Features {
+@JvmName("camera")
+fun PackageManager.hasCameraFeature(): Boolean {
+    return hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)
+}
 
-    public static boolean phone(PackageManager pm) {
-        return pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
+@JvmName("phone")
+fun PackageManager.hasPhoneFeature(): Boolean {
+    return hasSystemFeature(PackageManager.FEATURE_TELEPHONY)
+}
+
+@JvmName("sms")
+fun PackageManager.hasSmsFeature(): Boolean {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        hasSystemFeature(PackageManager.FEATURE_TELEPHONY_MESSAGING)
+    } else {
+        hasSystemFeature(PackageManager.FEATURE_TELEPHONY)
     }
+}
 
-    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
-    public static boolean sms(PackageManager pm) {
-        return pm.hasSystemFeature(PackageManager.FEATURE_TELEPHONY_MESSAGING);
-    }
-
-    public static boolean torch(PackageManager pm) {
-        // todo: it'd be a good idea to test this on a minSdk device.
-        return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-    }
-
-    private Features() {}
+@JvmName("torch")
+fun hasTorchFeature(pm: PackageManager): Boolean {
+    // todo: it'd be a good idea to test this on a minSdk device.
+    return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)
 }
