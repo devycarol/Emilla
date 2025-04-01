996dd1f11f3fc1ae4a9368ad0be2b34b59251050
devycarol <[email protected]>
2025-03-31 22:10:25 -0600

    reorganize number wrappers

--- app/src/main/java/net/emilla/math/IntegerNumber.java
+++ app/src/main/java/net/emilla/math/IntegerNumber.java
@@ -1,17 +1,12 @@
 package net.emilla.math;
 
-import static net.emilla.math.Maths.malformedExpression;
-import static java.lang.Long.parseLong;
-
 import androidx.annotation.StringRes;
 
 /*internal*/ final class IntegerNumber implements BitwiseToken {
 
     final long val;
 
-    IntegerNumber(String num, @StringRes int errorTitle) { try {
-        val = parseLong(num);
-    } catch (NumberFormatException e) {
-        throw malformedExpression(errorTitle);
-    }}
+    IntegerNumber(String num, @StringRes int errorTitle) {
+        val = Maths.tryParseLong(num, errorTitle);
+    }
 }
--- app/src/main/java/net/emilla/math/Maths.kt
+++ app/src/main/java/net/emilla/math/Maths.kt
@@ -10,6 +10,18 @@ import java.text.DecimalFormat
 // todo: configurable sig digs.
 fun prettyNumber(n: Double): String = DecimalFormat("#.######").format(n)
 
+fun tryParseLong(num: String, errorTitle: Int) = try {
+    num.toLong()
+} catch (_: NumberFormatException) {
+    throw malformedExpression(errorTitle)
+}
+
+fun tryParseDouble(num: String, errorTitle: Int) = try {
+    num.toDouble()
+} catch (_: NumberFormatException) {
+    throw malformedExpression(errorTitle)
+}
+
 fun malformedExpression(@StringRes errorTitle: Int): EmillaException {
     return EmillaException(errorTitle, R.string.error_calc_malformed_expression)
 }
