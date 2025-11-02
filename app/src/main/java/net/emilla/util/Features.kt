c7ee32f11819654e2c96c43b0a554e86f6b7bd52
devycarol <[email protected]>
2025-11-01 20:26:13 -0600

    expression body style

--- app/src/main/java/net/emilla/util/Features.kt
+++ app/src/main/java/net/emilla/util/Features.kt
@@ -6,14 +6,12 @@ import android.content.pm.PackageManager
 import android.os.Build
 
 @JvmName("camera")
-fun PackageManager.hasCameraFeature(): Boolean {
-    return hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)
-}
+fun PackageManager.hasCameraFeature(): Boolean
+    = hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)
 
 @JvmName("phone")
-fun PackageManager.hasPhoneFeature(): Boolean {
-    return hasSystemFeature(PackageManager.FEATURE_TELEPHONY)
-}
+fun PackageManager.hasPhoneFeature(): Boolean
+    = hasSystemFeature(PackageManager.FEATURE_TELEPHONY)
 
 @JvmName("sms")
 fun PackageManager.hasSmsFeature(): Boolean {
@@ -25,7 +23,6 @@ fun PackageManager.hasSmsFeature(): Boolean {
 }
 
 @JvmName("torch")
-fun hasTorchFeature(pm: PackageManager): Boolean {
+fun hasTorchFeature(pm: PackageManager): Boolean
+    = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)
     // todo: it'd be a good idea to test this on a minSdk device.
-    return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)
-}
