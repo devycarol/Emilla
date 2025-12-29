c8292245a7ec77130b0ebcc4e6347031867a37d9
devycarol <[email protected]>
2025-12-29 09:14:46 -0700

    dekotlinize once and for all

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -24,20 +23,22 @@ android {
         release {
             isMinifyEnabled = true
             isShrinkResources = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
+
     buildFeatures {
         viewBinding = true
         buildConfig = true
-    }
-    kotlinOptions {
-        jvmTarget = "17"
     }
 }
 
@@ -45,14 +46,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.8.8")
-    implementation("androidx.navigation:navigation-ui-ktx:2.8.8")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.0"))
-    implementation("androidx.preference:preference-ktx:1.2.1")
-    implementation("androidx.activity:activity-ktx:1.10.1")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-livedata:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel:2.8.7")
+    implementation("androidx.navigation:navigation-fragment:2.8.8")
+    implementation("androidx.navigation:navigation-ui:2.8.8")
+    implementation("androidx.preference:preference:1.2.1")
+    implementation("androidx.activity:activity:1.10.1")
+    implementation("androidx.core:core:1.15.0")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
---
+++ app/src/main/java/net/emilla/ping/Pings.java
@@ -0,0 +1,31 @@
+package net.emilla.ping;
+
+import android.app.Notification;
+import android.content.Context;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+public enum Pings {
+    ;
+
+    public static Notification make(
+        Context ctx,
+        PingChannel channel,
+        CharSequence title,
+        @Nullable CharSequence text,
+        @DrawableRes int icon
+    ) {
+        var builder = new NotificationCompat.Builder(ctx, channel.id)
+            .setContentTitle(title)
+            .setSmallIcon(icon);
+
+        if (text != null) {
+            builder.setContentText(text);
+        }
+
+        return builder.build();
+    }
+
+}
